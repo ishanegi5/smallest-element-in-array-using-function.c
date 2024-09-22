f8b9d24ba72d14086a724b1af72101ac0dafaef7
Isha Negi <itsishanegi@gmail.com>
2024-09-22 18:47:09 +0530

    Add files via upload

---
+++ smallest-element-in-array-using-function.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int smallest()
+
+{ int arr[]={20,18,14,26};
+    
+int min=arr[0];
+    
+for(int i=0;i<4;i++)
+    
+{ if(min>arr[i])
+    
+{
+        
+min=arr[i];
+    
+}
+    
+}
+    
+printf("Smallest number in array= %d",min);
+    
+return 0;
+
+}
+
+int main()
+
+{ smallest(); 
+  
+return  0;
+
+}
